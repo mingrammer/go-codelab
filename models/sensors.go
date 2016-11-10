3a5cbb599dde6cba4bd05e4e217bab702d74b27d
mingrammer <k239507@gmail.com>
2016-11-10 17:25:25 +0900

    Rewrite the comments for sensor structs

--- models/sensors.go
+++ models/sensors.go
@@ -2,13 +2,14 @@ package models
 
 import "time"
 
+// Sensor has common fields for any sensors
 type Sensor struct {
 	Name    string    `json:"name"`
 	Type    string    `json:"type"`
 	GenTime time.Time `json:"gen_time"`
 }
 
-// Struct for Gyro Sensor
+// GyroSensor produces x-y-z axes angle velocity values
 type GyroSensor struct {
 	Sensor
 	AngleVelocityX float32 `json:"x_axis_angle_velocity"`
@@ -16,7 +17,7 @@ type GyroSensor struct {
 	AngleVelocityZ float32 `json:"z_axis_angle_velocity"`
 }
 
-// Struct for Accelerometer Sensor
+// AccelSensor produces x-y-z axes gravity acceleration values
 type AccelSensor struct {
 	Sensor
 	GravityAccX float32 `json:"x_axis_gravity_acceleration"`
@@ -24,7 +25,7 @@ type AccelSensor struct {
 	GravityAccZ float32 `json:"z_axis_grativy_acceleration"`
 }
 
-// Struct for Temperature Sensor
+// TempSensor produces temperature and humidity values
 type TempSensor struct {
 	Sensor
 	Temperature float32 `json:"temperature"`
