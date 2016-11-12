8eda5a1e6afc21ad5231db1e6ade6e772df9a263
mingrammer <k239507@gmail.com>
2016-11-12 13:42:13 +0900

    Add comments to all functions

--- faker/range.go
+++ faker/range.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// getRandValueInRange creates random value in (range size - error, range size + error)
 func getRandValueInRange(rangeSize int, epsilon float64) float64 {
 	rand.Seed(time.Now().UTC().UnixNano())
 
@@ -15,18 +16,22 @@ func getRandValueInRange(rangeSize int, epsilon float64) float64 {
 	return dataInRange
 }
 
+// GenerateAngleVelocity generates random value in (0 - epsilon, 360 + epsilon)
 func GenerateAngleVelocity(epsilon float64) float64 {
 	return getRandValueInRange(360, epsilon)
 }
 
+// GenerateGravityAcceleration generates random value in (0 - epsilon, 1023 + epsilon)
 func GenerateGravityAcceleration(epsilon float64) float64 {
 	return getRandValueInRange(1023, epsilon)
 }
 
+// GenerateTemperature generates random value in (0 - epsilon, 104 + epsilon)
 func GenerateTemperature(epsilon float64) float64 {
 	return getRandValueInRange(104, epsilon)
 }
 
+// GenerateHumidity generates random value in (0 - epsilon, 100 + epsilon)
 func GenerateHumidity(epsilon float64) float64 {
 	return getRandValueInRange(100, epsilon)
 }
