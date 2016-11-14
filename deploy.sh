b0c03cfd7eea64b2260df9a6d3d329cd8f64aafc
mingrammer <k239507@gmail.com>
2016-11-15 00:24:11 +0900

    Rebuilding site 2016년 11월 15일 화요일 00시 24분 11초 KST

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+cd docs && hugo
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="Rebuilding site `date`"
+if [ $# -eq 1 ]
+      then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+cd ../
+git push origin master
+git subtree push --prefix=docs/public git@github.com:mingrammer/go-codelab.git gh-pages
